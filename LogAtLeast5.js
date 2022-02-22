08292912a009914d96bfc91a594d5ba039fdce0b
mdubravski <mdubravski@gmail.com>
2022-02-22 16:45:11 -0500

    remove files

--- LogAtLeast5.js
+++
@@ -1,9 +0,0 @@
-/**
- *  O(n)
- */
-function logAtLeast5(n){
-  for(let i=1; i <= Math.max(5,n); i++){
-    console.log(i);
-  }
-}
-console.log(logAtLeast5(4));
--- LogAtMost5.js
+++
@@ -1,9 +0,0 @@
-/**
- * O(1)
- */
-function logAtMost5(n){
-  for(let i=1; i<=Math.min(5,n); i++){
-    console.log(i);
-  }
-}
-console.log(logAtMost5(3));
