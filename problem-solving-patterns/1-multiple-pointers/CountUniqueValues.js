083a9dab82c69d70de36c718ced1fa4b69a82b9a
mdubravski <mdubravski@gmail.com>
2022-02-26 09:17:57 -0500

    add code examples

--- problem-solving-patterns/1-multiple-pointers/CountUniqueValues.js
+++ problem-solving-patterns/1-multiple-pointers/CountUniqueValues.js
@@ -2,6 +2,7 @@
  * Implement a function countUniqueValues which accepts a sorted array,
  * and counts the unique values in the array. There can be negative 
  * numbers in the array, but it will always be sorted.
+ * Return the number of unique values.
  */
 
 // naiveCountUniqueValues(arr){
@@ -17,8 +18,11 @@
 /**
  * time => O(n)
  * space => O(n)
+ * this one returns the number of values that are not duplicates
  */
 function fqCountUniqueValues(arr) {
+  if (arr.length === 0) return 0;
+
   let obj = {};
   let ret = [];
   for (let n of arr) {
@@ -27,20 +31,44 @@ function fqCountUniqueValues(arr) {
   for (let k in obj) {
     if (obj[k] === 1) ret.push(k);
   }
-  return ret;
+  return ret.length;
 }
 
+/**
+ * 
+ * time => O(n)
+ * space => O(n)
+ * 
+ */
+function fqCountUniqueValues1(arr) {
+  if (arr.length === 0) return 0;
+
+  let obj = {};
+  for(let n of arr){
+    obj[n] =(obj[n] || 0) + 1;
+  }
+  return Object.keys(obj).length;
+}
+
+/**
+ * 
+ * time => O(n)
+ * space => O(n)
+ * 
+ */
 function mpCountUniqueValues(arr) {
+  if (arr.length === 0) return 0;
+
   let i = 0;
-  for(let j=1; j<arr.length; j++){
-    if(arr[i] !== arr[j]){
+  for (let j = 1; j < arr.length; j++) {
+    if (arr[i] !== arr[j]) {
       i++;
       arr[i] = arr[j];
     }
-    console.log(i,j);
   }
   return i+1;
 }
-
-console.log(fqCountUniqueValues([-2, -1, -1, 1, 1, 2, 3, 3, 4, 4, 4, 5, 6, 8, 8]));
-console.log(mpCountUniqueValues([-2, -1, -1, 1, 1, 2, 3, 3, 4, 4, 4, 5, 6, 8, 8]));
+                           //          i            j 
+console.log(fqCountUniqueValues([3, 3, 3, 4, 4, 4, 5, 5, 5, 5, 6]));
+console.log(fqCountUniqueValues1([3, 3, 3, 4, 4, 4, 5, 5, 5, 5, 6]));
+console.log(mpCountUniqueValues([3, 3, 3, 4, 4, 4, 5, 5, 5, 5, 6]));
