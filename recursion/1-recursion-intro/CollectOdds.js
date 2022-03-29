813c071956860dadf9ae53d24b14068c9e529a99
mdubravski <mdubravski@gmail.com>
2022-03-29 08:55:52 -0400

    add examples

--- recursion/1-recursion-intro/CollectOdds.js
+++ recursion/1-recursion-intro/CollectOdds.js
@@ -1,4 +1,8 @@
-function collectOdds(arr){
+/**
+ * Helper Method Recursion 
+ *  - pattern where outer non-recursive function calls inner recursive function
+ */
+function collectOddsHR(arr){
   let result = [];
 
   function helper(nums){
@@ -14,4 +18,20 @@ function collectOdds(arr){
   return result;
 }
 
-console.log(collectOdds([1,2,3,4,5]));
+/**
+ * Pure Recursion 
+ *  - no helper method or external ds in function
+ */
+function collectOddsPR(arr){
+  let odds = [];
+
+  // base case
+  if(arr.length === 0) return odds;
+  // diff input
+  if(arr[0] % 2 !== 0) odds.push(arr[0]);
+
+  odds = odds.concat(collectOddsPR(arr.slice(1)));
+  return odds;
+}
+
+console.log(collectOddsPR([1,2,3,4,5]));
